5715abce38e65adb8d17fd58bd9e69913c566b1c
liam <liammaina143@gmail.com>
2025-09-24 07:36:43 +0300

    added level 2 and organised files
    organised files into separate folders and added a level 2 scene.

--- scripts/playerMovement.gd
+++ scripts/playerMovement.gd
@@ -9,22 +9,22 @@ var isDashing :bool = false
 func _physics_process(delta: float) -> void:
 	var direction := Vector2.ZERO
 	
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("right"):
 		direction.x += 1
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("left"):
 		direction.x += -1
 	
 	if not isDashing:
 		velocity.x = direction.x * movementSpeed 
 	
-	if Input.is_action_pressed("ui_up") and is_on_floor():
+	if Input.is_action_pressed("up") and is_on_floor():
 		velocity.y = -jumpForce
 	
 	# Apply gravity
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		
-	if Input.is_action_just_pressed("ui_accept") and direction.x != 0:
+	if Input.is_action_just_pressed("dash") and direction.x != 0:
 		dash(direction)
 
 		
